678f5ca8e732fe1cc620b5e096cf5241af36d24a
Harshavardhana <harsha4688v@gmail.com>
2023-09-12 19:53:46 +0530

    ngw fix

--- vpc.tf
+++ vpc.tf
@@ -30,7 +30,7 @@ resource "aws_eip" "elastic-ip" {
 }
 
 resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.nat_gateway.id
+  allocation_id = aws_eip.elastic-ip.id
   subnet_id     = aws_subnet.private.id
 
   tags = {

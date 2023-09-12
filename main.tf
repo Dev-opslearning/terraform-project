ea85bfced7d2a60a71104b6af3110b1e1b5d4083
Harshavardhana <harsha4688v@gmail.com>
2023-09-12 19:45:15 +0530

    4th

--- main.tf
+++ main.tf
@@ -8,11 +8,10 @@ resource "aws_instance" "private_ec2" {
   key_name      = "ec2-keypair"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private.id
-}
 tags = {
     name = "private-ec2"
 }
-
+}
 
 resource "aws_instance" "public_ec2" {
   ami           = "ami-053b0d53c279acc90"
